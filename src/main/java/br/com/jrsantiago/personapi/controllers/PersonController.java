c311bd6587bae6f96d8aad9ae8d94ff5a9ab7e3e
José Reis R Santiago <jsribeirosantiago@gmail.com>
2021-05-18 01:34:28 -0300

    Ateração realizada na classe de controle de uma pessoa, onde foi adequada o metodo de criação de uma pessoa via verbo POST.

--- src/main/java/br/com/jrsantiago/personapi/controllers/PersonController.java
+++ src/main/java/br/com/jrsantiago/personapi/controllers/PersonController.java
@@ -1,16 +1,29 @@
 package br.com.jrsantiago.personapi.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.jrsantiago.personapi.dto.MessageResponseDTO;
+import br.com.jrsantiago.personapi.models.Person;
+import br.com.jrsantiago.personapi.services.PersonService;
 
 @RestController
 @RequestMapping("/api/v1/pessoas")
 public class PersonController {
-    
-    @GetMapping
-    public String getPeople() {
-        return "API REST Person";
+    @Autowired
+    private PersonService personService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public MessageResponseDTO create(@RequestBody Person person) {
+        Person savedPerson = personService.save(person);
+
+        return MessageResponseDTO.builder().message("Pessoa criada com ID " + savedPerson.getId()).build();
     }
 
 }
